1a63ed7504096001e9b99260be9f3598a624e20a
Jonathan Kitt <jonathan.kitt@inrae.fr>
2022-08-29 15:52:36 +0200

    finished 1st chapter

--- app/app.R
+++ app/app.R
@@ -1,10 +1,26 @@
 library(shiny)
 
 ui <- fluidPage(
-  "Hello, world!"
+  selectInput(inputId = "dataset",
+              label = "Dataset",
+              choices = ls("package:datasets")),
+  verbatimTextOutput(outputId = "summary"),
+  tableOutput(outputId = "table")
 )
 
 server <- function(input, output, session) {
+  
+  dataset <- reactive({
+    get(input$dataset, "package:datasets")
+  })
+  
+  output$summary <- renderPrint({
+    summary(dataset())
+  })
+  
+  output$table <- renderTable({
+    dataset()
+  })
 }
 
 shinyApp(ui, server)
